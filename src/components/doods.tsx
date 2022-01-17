e2fa411af903a07d1ec4f699a26eba469943ae63
ekrocks21 <erickane21@gmail.com>
2022-01-17 16:10:34 -0500

    ui updates

--- src/components/doods.tsx
+++ src/components/doods.tsx
@@ -40,7 +40,7 @@ export default function BasicGrid() {
               Each quarter, 100 of you Good Doods will be rewarded with the gifts featured in your GDGC
               membership card.  
             </ p>    
-            <a href=" https://trello.com/goodgoodgivingclub" target="blank" style={{ color: "#a8cfe5", display: "flex", justifyContent: "left", textAlign: "justify", fontSize:"18px", paddingTop: "15px" }}>
+            <a href=" https://docs.google.com/document/d/1gSUQF4KOXTVoA97vpnZ2Oy0A7HXB6kXx5pgGTVZxa3g/edit?usp=sharing" target="blank" style={{ color: "#a8cfe5", display: "flex", justifyContent: "left", textAlign: "justify", fontSize:"18px", paddingTop: "15px" }}>
 
 Explore our giving model
     
