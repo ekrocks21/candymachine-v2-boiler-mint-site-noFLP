85db1c57f1b22554baa23cda34a58322b995911a
ekrocks21 <erickane21@gmail.com>
2022-01-18 16:34:29 -0500

    ui updates

--- src/Minter.tsx
+++ src/Minter.tsx
@@ -41,7 +41,7 @@ const ConnectButton = styled(WalletDialogButton)`
   height: 60px;
   margin-top: 10px;
   margin-bottom: 5px;
-  background: linear-gradient(180deg, #604ae5 0%, #813eee 100%);
+  background: linear-gradient(180deg, #008080 20%, #478181 80%);
   color: white;
   font-size: 16px;
   font-weight: bold;
